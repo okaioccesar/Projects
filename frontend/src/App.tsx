d4147438b812d4bf95e14e1c75eafc7346bb73e7
Kaio Cesar <kaiocesareng18@gmail.com>
2026-08-28 05:33:27 +0000

    Adiciona estrutura inicial de sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,25 @@ function App() {
         <main>
   <h1>Gym Manager</h1>
   <p>Sistema de gerenciamento de academia</p>
+
+  <aside>
+    Gym Manager
+
+    <nav>
+      <ul>
+        <li>Dashboard</li>
+        <li>Alunos</li>
+        <li>Treinos</li>
+        <li>Planos</li>
+        <li>Professores</li>
+      </ul>
+    </nav>
+  </aside>
+
+  <section>
+    <h1>Dashboard</h1>
+  </section>
+  
 </main>
   )
 }
